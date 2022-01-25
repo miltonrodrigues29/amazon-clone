cb144157134518c7f18ac54b3b9cc232c8a9d65e
Milton Rodrigues <26818@telxsids.net>
2022-01-25 22:32:37 +0530

    server created

---
+++ backend/data.js
@@ -0,0 +1,80 @@
+const data = {
+    products:
+    [
+      {
+          _id:'1',
+          name:'Nike slim Shirt',
+          category:"Shirts",
+          image:"/images/p1.jpg",
+          price:120,
+          countInStock:22,
+          brand:"Nike",
+          rating:4.5,
+          numReviews:10,
+          description:'high quality product'
+      },
+      {
+        _id:'2',
+        name:'Adidas fit Shirt',
+        category:"Shirts",
+        image:"/images/p2.jpg",
+        price:125,
+        countInStock:22,
+        brand:"Adidas",
+        rating:4.0,
+        numReviews:11,
+        description:'ok quality product'
+    },
+    {
+        _id:'3',
+        name:'Puma slim Shirt',
+        category:"Shirts",
+        image:"/images/p3.jpg",
+        price:128,
+        countInStock:22,
+        brand:"Puma",
+        rating:4.5,
+        numReviews:15,
+        description:'nice quality product'
+    },
+    {
+        _id:'4',
+        name:'HH slim Shirt',
+        category:"Shirts",
+        image:"/images/p4.jpg",
+        price:120,
+        countInStock:22,
+        brand:"HH",
+        rating:4.5,
+        numReviews:1,
+        description:'good product'
+    },
+    {
+        _id:'5',
+        name:'Nike Shirt',
+        category:"Shirts",
+        image:"/images/p5.jpg",
+        price:120,
+        countInStock:22,
+        brand:"Nike",
+        rating:4.5,
+        numReviews:10,
+        description:'high quality product'
+    },
+     {
+          _id:'6',
+          name:'Rarerabbit slim Shirt',
+          category:"Shirts",
+          image:"/images/p6.jpg",
+          price:120,
+          countInStock:0,
+          brand:"Rare Rabbit",
+          rating:5,
+          numReviews:100,
+          description:'Dope quality product'
+      },
+
+    ]
+}
+
+export default data;
