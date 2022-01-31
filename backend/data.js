9d877bb0b7ed53ee821fe7e21ff0446426671e7e
Milton Rodrigues <26818@telxsids.net>
2022-01-31 13:57:24 +0530

    connection to mongodb,express async error handler, data excess form mongodb complete

--- backend/data.js
+++ backend/data.js
@@ -1,4 +1,22 @@
+import bcrypt from 'bcryptjs'
+
 const data = {
+  users:[
+    {
+      name: 'Milton',
+      email: 'admin@example.com',
+      //since it is not safe to keep passwords in plain texts in server, we encrypt it using bcrypr and add salt of 8
+      password: bcrypt.hashSync('123456',8),
+      isAdmin: true,
+    },
+    {
+      name: 'John',
+      email: 'user@example.com',
+      //since it is not safe to keep passwords in plain texts in server, we encrypt it using bcrypr and add salt of 8
+      password: bcrypt.hashSync('123456',8),
+      isAdmin: false,
+    }
+  ],
   products: [
     {
       _id: '1',
--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,14 @@
 import express from 'express';
+import mongoose from 'mongoose';
 import data from './data.js';
+import userRouter from './routers/userRouter.js';
+
 
 const app = express();
+
+mongoose.connect(process.env.MONGODB_URL ||  'mongodb://localhost/amazona')
+
+//last amazona represents the database name
 
 app.get('/api/products/:id', (req, res) => {
   const product = data.products.find((x) => x._id === req.params.id);
@@ -16,9 +23,19 @@ app.get('/api/products', (req, res) => {
   res.send(data.products);
 });
 
+app.use('/api/users',userRouter); //for this prefic userRouter should respond, i.e if it is followed by /seed than it should call create user api
+
 app.get('/', (req, res) => {
   res.send('Server is ready');
 });
+
+
+//error catcher, handles error occured in router functions wrapped in express-async-handler.
+app.use((err,req,res,next)=>
+{
+  res.status(500).send({message:err.message})
+})
+
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log(`Serve at http://localhost:${port}`);
